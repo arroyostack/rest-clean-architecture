60d49e3904677b3044df847e9355ac86f8bdc334
arroyostack <arroyostack@gmail.com>
2024-02-02 13:11:03 +0100

    Feat: envs.

---
+++ src/presentation/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+
+export class Server {
+    private app = express();
+
+    async start() {
+
+        // Middleware
+
+        // Public folder content.
+        this.app.use( express.static( 'public' ) );
+
+        this.app.get( "*", ( req, res ) => {
+            const indexPath = path.join( __dirname + '../../../public/index.html' );
+            res.sendFile( indexPath );
+        } );
+
+        this.app.listen( 3000, () => {
+            console.log( `Server running on port ${3000}` );
+        } );
+    }
+}
